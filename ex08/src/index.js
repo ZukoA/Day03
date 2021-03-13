e6ca83d63dd0fee2ee0e177d188995c8ff4b8de5
ZukoA <azerinazuko@gmail.com>
2021-03-13 13:04:12 +0100

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var sum;
+    sum = numOne + numTwo;
+
+    // Only change code below this line
+    return sum;
+}
+
+console.log(main(15, 8)); //change this line
+console.log(main(5, 28)); //change this line
+console.log(main(150, 425)); //change this line
+
+module.exports = main;
