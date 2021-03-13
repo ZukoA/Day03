853cda03afd91c0b1eea2bf9c6ce4efcbbd3ff46
ZukoA <azerinazuko@gmail.com>
2021-03-13 13:08:51 +0100

    ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var difference;
+    sum = numOne - numTwo;
+
+    // Only change code below this line
+    return difference;
+}
+
+console.log(main(15, 8)); //change this line
+console.log(main(25, 8)); //change this line
+console.log(main(425, 120)); //change this line
+
+module.exports = main;
