85f4fad8485c893a16897490cf2bc84286b72822
ZukoA <azerinazuko@gmail.com>
2021-03-13 12:15:58 +0100

    ex07

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,8 +1,8 @@
 function main() {
     // Only change code below this line
-    var x = 9;
-    var y = 16;
-    var z = "My name is John!";
+    var x = 7;
+    var y = 12;
+    var z = "My name is ";
     // Only change code below this line
     x = x + 2;
     y = y + 4;
